490e2d03a6af9f059b5e11f8fb1d69e60f7aefa2
Bobby Rosenberg <34346749+marzton@users.noreply.github.com>
2026-02-15 03:40:13 -0500

    Revert "Update gitignore (#1529)"
    This reverts commit 9bc442462cd7d1f47978cec0de54f9cb61d76f25.

--- apps/gs-web/src/pages/api/forms/[slug].ts
+++ apps/gs-web/src/pages/api/forms/[slug].ts
@@ -129,4 +129,6 @@ export const PUT: APIRoute = async ({ request, locals, params }) => {
 
 export const PATCH = PUT;
 
+export const prerender = false;
+
 export const getStaticPaths = async () => [];
